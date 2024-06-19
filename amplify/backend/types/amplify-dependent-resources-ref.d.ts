28b1d0ad4f5751fb0d5cd4c8717a09f79e859342
Phạm Thanh Hùng <hungp0722@gmail.com>
2024-06-19 13:54:54 +0700

    install and setup Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "cdslcrm": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
