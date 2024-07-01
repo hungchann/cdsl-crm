5839d22875c40eea98551e9b80b091a6c0432bfd
Phạm Thanh Hùng <hungp0722@gmail.com>
2024-07-01 10:58:16 +0700

    delete api because bug

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,17 +1,12 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "cdslcrm": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
   "auth": {
     "cdslcrme422e372": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
